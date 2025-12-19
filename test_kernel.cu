e7b7cd191de398cd8de61a49ce83092b8edd5c7c
hek14 <1023129548@qq.com>
2025-12-19 14:30:31 +0800

    fix: correct kernel input type and simplify launch function in test_kernel.cu

--- test_kernel.cu
+++ test_kernel.cu
@@ -6,13 +6,13 @@ static float** cached_d_ptrs = nullptr;
 static int    cached_batch  = 0;
 static std::vector<float*> cached_host_ptrs = {};
 
-__global__ void add_kernel(float** in, float* out, int n, int batch)
+__global__ void add_kernel(float* in, float* out, int n, int batch)
 {
     int tid = blockIdx.x * blockDim.x + threadIdx.x; 
     if(tid < n){
         float sum = 0.0f;
         for(int i = 0; i < batch; ++i){
-            sum += in[i][tid];
+            sum += in[tid];
         }
         out[tid] = sum;
     }
@@ -29,19 +29,20 @@ void launch(const std::vector<torch::Tensor>& in_tensors, torch::Tensor out)
     }
     dim3 numThreads = {1024};
     dim3 numBlocks = {(size_t)((n + 1024 - 1) / 1024)};
-    printf("%d %d\n", n, batch);
-    // reuse or initialize cached device pointer array via managed memory
-    if (cached_batch != batch || ptrs != cached_host_ptrs) {
-        if (cached_d_ptrs) cudaFree(cached_d_ptrs);
-        cudaMallocManaged((void**)&cached_d_ptrs, batch * sizeof(float*));
-        for (int i = 0; i < batch; ++i) {
-            cached_d_ptrs[i] = ptrs[i];
-        }
-        cudaDeviceSynchronize(); // ensure pointers are updated on device
-        cached_batch = batch;
-        cached_host_ptrs = ptrs;
-    }
-    add_kernel<<<numBlocks, numThreads>>>(cached_d_ptrs, out.data_ptr<float>(), n, batch);
+    // printf("%d %d\n", n, batch);
+    // // reuse or initialize cached device pointer array via managed memory
+    // if (cached_batch != batch || ptrs != cached_host_ptrs) {
+    //     printf("re-allocate\n");
+    //     if (cached_d_ptrs) cudaFree(cached_d_ptrs);
+    //     cudaMallocManaged((void**)&cached_d_ptrs, batch * sizeof(float*));
+    //     for (int i = 0; i < batch; ++i) {
+    //         cached_d_ptrs[i] = ptrs[i];
+    //     }
+    //     cudaDeviceSynchronize(); // ensure pointers are updated on device
+    //     cached_batch = batch;
+    //     cached_host_ptrs = ptrs;
+    // }
+    add_kernel<<<numBlocks, numThreads>>>(in_tensors[0].data_ptr<float>(), out.data_ptr<float>(), n, batch);
 }
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
